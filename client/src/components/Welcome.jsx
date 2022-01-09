3804c72ed4951148023462d86fbdcac726f06878
SuperSahil <sahilv550@gmail.com>
2022-01-09 23:27:39 +0530

    Custom Everything

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -35,10 +35,10 @@ const Welcome = () => {
             <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
                 <div className='flex flex-1 justify-start flex-col mf:mr-10'>
                     <h1 className='text-3xl sm:text-5xl text-white text-gradient py-1'>
-                        Send Crypto <br />across the world
+                        Send Crypto <br />across the world <br/> (Rapson network)
                     </h1>
                     <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
-                        Explore the crypt worl, bns easily on Krypto
+                        Just connect to a Rapson network and start sending crypto !
                     </p>
                     {!currentAccount && (
                         <button
